ec52fc22caddc5486c9e2f42644bb752d9c45471
Maxime Vidori <maxime.vidori@gmail.com>
2014-12-11 18:34:29 +0100

    Add tests for /

--- src/db/connector.py
+++ src/db/connector.py
@@ -1,15 +1,18 @@
 import os
 import peewee
 
-with open('password') as f:
-    password = f.readline()
+try:
+    with open('password') as f:
+        password = f.readline()
+except:
+    password = 'password'
 
 # Connect to the database URL defined in the environment with docker
 config = {
     'database': 'rulzurdb',
     'user': 'rulzurdb',
-    'host': os.environ['RULZURDB_PORT_5432_TCP_ADDR'],
-    'port': os.environ['RULZURDB_PORT_5432_TCP_PORT'],
+    'host': os.environ.get('RULZURDB_PORT_5432_TCP_ADDR'),
+    'port': os.environ.get('RULZURDB_PORT_5432_TCP_PORT'),
     'password': password.rstrip('\n'),
 }
 
--- test/api/endpoints_test.py
+++ test/api/endpoints_test.py
@@ -1,11 +1,17 @@
 import pytest
 import api
+import json
 
-@pytest.fixture
+@pytest.fixture()
 def app():
     api.app.config['TESTING'] = True
     return api.app.test_client()
 
-def test_root(app):
+def test_root(app, monkeypatch):
+    def list_table_mock(schema):
+        assert schema == 'rulzurkitchen'
+        return ['table1', 'table2']
+
+    monkeypatch.setattr('db.connector.database.get_tables', list_table_mock)
     root = app.get('/')
-    assert 'Hello World!' in root.data
+    assert json.loads(root.data) == {'tables': ['table1', 'table2']}
