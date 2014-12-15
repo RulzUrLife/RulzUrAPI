db7c76162df84a6c69fcf8c0afee22a3b231fb3e
Maxime Vidori <maxime.vidori@gmail.com>
2014-12-15 17:02:24 +0100

    Enum field
        Implement an enum field for postgres, adapt the model in consequence

---
+++ src/db/enum.py
@@ -0,0 +1,34 @@
+# Enum field cloned and (not) adapted from
+# https://gist.github.com/b1naryth1ef/607e92dc8c1748a06b5d
+from peewee import *
+
+class EnumField(Field):
+    db_field = "enum"
+
+    def pre_field_create(self, model):
+        field = "e_%s" % self.name
+
+        self.get_database().get_conn().cursor().execute(
+            "DROP TYPE IF EXISTS %s;" % field
+        )
+
+        query = self.get_database().get_conn().cursor()
+
+        tail = ', '.join(["'%s'"] * len(self.choices)) % tuple(self.choices)
+        q = "CREATE TYPE %s AS ENUM (%s);" % (field, tail)
+        query.execute(q)
+
+    def post_field_create(self, model):
+        self.db_field = "e_%s" % self.name
+
+    def coerce(self, value):
+        if value not in self.choices:
+            raise Exception("Invalid Enum Value `%s`", value)
+        return str(value)
+
+    def get_column_type(self):
+        return "enum"
+
+    def __ddl_column__(self, ctype):
+        return SQL("e_%s" % self.name)
+
--- src/db/models.py
+++ src/db/models.py
@@ -2,9 +2,21 @@ import peewee
 import playhouse.postgres_ext
 
 import db.connector
+import db.enum
 
 
 class BaseModel(peewee.Model):
+    @classmethod
+    def create_table(cls, *args, **kwargs):
+        for field in cls._meta.get_fields():
+            if hasattr(field, "pre_field_create"):
+                field.pre_field_create(cls)
+
+        cls._meta.database.create_table(cls)
+
+        for field in cls._meta.get_fields():
+            if hasattr(field, "post_field_create"):
+                field.post_field_create(cls)
 
     class Meta:
         database = db.connector.database
@@ -26,9 +38,11 @@ class Recipe(BaseModel):
     name = peewee.CharField()
     directions = playhouse.postgres_ext.JSONField()
     difficulty = peewee.IntegerField()
-    duration = peewee.CharField()
+    duration = db.enum.EnumField(choices=[
+        '0/5', '5/10', '10/15', '15/20', '20/25', '25/30', '30/45', '45/60',
+        '60/75', '75/90', '90/120', '120/150'])
     people = peewee.IntegerField()
-    type = peewee.CharField()
+    type = db.enum.EnumField(choices=['starter', 'main', 'dessert'])
 
 
 class RecipeIngredients(BaseModel):
@@ -41,7 +55,7 @@ class RecipeIngredients(BaseModel):
             related_name='recipes',
             db_column='fk_ingredient')
     quantity = peewee.IntegerField()
-    measurement = peewee.CharField()
+    measurement = db.enum.EnumField(choices=['L', 'g', 'oz', 'spoon'])
 
     class Meta:
         primary_key = peewee.CompositeKey('recipe', 'ingredient')
